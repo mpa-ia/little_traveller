bf634a352a8e4b78cb3e31e847c9aa7c667d2d42
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-25 22:14:17 +0100

    Header - distinguish header text part, change image, adjust styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './Header.scss';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 const Header = props => (
     <header className={styles.component}>
-        <h1 className={styles.title}>{props.header}</h1>
+        <h1 className={styles.title}>{ReactHtmlParser(props.header)}</h1>
         <img className={styles.image} src={props.background}/>
     </header>
 );
 
 Header.propTypes = {
-    header: PropTypes.string,
+    header: PropTypes.node,
     background: PropTypes.string,
 };
 export default Header;
--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -1,4 +1,4 @@
 export const pageContent = {
-    headerTitle: 'Explore yourself',
-    headerBackground: 'https://images.pexels.com/photos/2104152/pexels-photo-2104152.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    headerTitle: 'Explore <small>yourself</small>',
+    headerBackground: 'https://images.pexels.com/photos/2887781/pexels-photo-2887781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 };
